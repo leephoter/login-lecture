02b6204ff9f313704e52c52e2d9f09b5626a46b8
leephoter <estarg51@gmail.com>
2021-12-28 10:20:20 +0900

    error installing nodemon

--- app/app.js
+++ app/app.js
@@ -2,12 +2,13 @@
 const express = require("express");
 const app = express();
 
-const home = require("./src/routes/home");
 // route
+const home = require("./src/routes/home");
 
 // app setting
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); //use >> middleware 등록
 
