8df0688d46dd27875c278309bbd7437de2df6d59
leephoter <estarg51@gmail.com>
2021-12-29 10:51:42 +0900

    회원가입 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -1,22 +1,28 @@
 "use strict";
 
 const UserStorage = require("./UserStorage");
+const { response } = require("express");
 
 class User {
     constructor(body) {
         this.body = body;
     }
     login() {
-        const body = this.body;
-        const { id, psword } = UserStorage.getUserInfo(body.id);
-        console.log("id :>> ", id);
+        const client = this.body;
+        const { id, psword } = UserStorage.getUserInfo(client.id);
         if (id) {
-            if (id === body.id && psword === body.psword) {
+            if (id === client.id && psword === client.psword) {
                 return { success: true };
             }
             return { success: false, msg: "비밀번호 오류" };
         }
         return { success: false, msg: "아이디 오류" };
+    }
+
+    register() {
+        const client = this.body;
+        const response = UserStorage.save(client);
+        return response;
     }
 }
 
--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,11 +9,16 @@ const id = document.querySelector("#id"),
 registerBtn.addEventListener("click", register);
 
 function register() {
+    if (!id.value) {
+        return alert("아이디 입력 필수");
+    }
+    if (psword !== confirmPsword) {
+        return alert("비밀번호 불일치");
+    }
     const req = {
         id: id.value,
         name: name.value,
         psword: psword.value,
-        confirmPsword: confirmPsword.value,
     };
     fetch("/register", {
         method: "POST",
